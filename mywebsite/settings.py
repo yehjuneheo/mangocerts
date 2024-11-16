dbb9bbb4cbeb11bf3b4bb455b06691307df1d14a
Yehjune Heo <yehjuneheo@Yehjunes-MacBook-Air.local>
2024-11-15 20:12:42 -0600

    production

--- mywebsite/settings.py
+++ mywebsite/settings.py
@@ -30,18 +30,18 @@ SECRET_KEY = os.getenv("SECRET_KEY")
 
 
 """!!!!!!!!!!!!!!DEPLOY WITH THIS!!!!!!!!!!!!!!!!!!!!"""
-#DEBUG = False
-#ALLOWED_HOSTS = ['mangocerts.com', '3.128.246.77', 'ec2-3-128-246-77.us-east-2.compute.amazonaws.com']
-#CSRF_TRUSTED_ORIGINS = ['https://earlyonmeetings.com', 'https://www.earlyonmeetings.com', 'https://3.128.12.57']
+DEBUG = False
+ALLOWED_HOSTS = ['mangocerts.com', '3.128.246.77', 'ec2-3-128-246-77.us-east-2.compute.amazonaws.com']
+CSRF_TRUSTED_ORIGINS = ['https://earlyonmeetings.com', 'https://www.earlyonmeetings.com', 'https://3.128.12.57']
 """!!!!!!!!!!!!!!DEPLOY WITH THIS!!!!!!!!!!!!!!!!!!!!"""
 
-DEBUG = True
-ALLOWED_HOSTS = ['mangocerts.com', '3.128.246.77', 'ec2-3-128-246-77.us-east-2.compute.amazonaws.com','127.0.0.1', 'localhost']
-CSRF_TRUSTED_ORIGINS = ['mangocerts.com', '3.128.246.77', 'ec2-3-128-246-77.us-east-2.compute.amazonaws.com', 'https://127.0.0.1', 'https://localhost']
+#DEBUG = True
+#ALLOWED_HOSTS = ['mangocerts.com', '3.128.246.77', 'ec2-3-128-246-77.us-east-2.compute.amazonaws.com','127.0.0.1', 'localhost']
+#CSRF_TRUSTED_ORIGINS = ['mangocerts.com', '3.128.246.77', 'ec2-3-128-246-77.us-east-2.compute.amazonaws.com', 'https://127.0.0.1', 'https://localhost']
 
-SECURE_SSL_REDIRECT = False
-SESSION_COOKIE_SECURE = False
-CSRF_COOKIE_SECURE = False
+SECURE_SSL_REDIRECT = True
+SESSION_COOKIE_SECURE = True
+CSRF_COOKIE_SECURE = True
 
 
 # Application definition
